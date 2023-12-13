4d85f106293cef5672a24f1d6eaf529a04aa978c
Han-Wanburhan <Hangster10761@gmail.com>
2023-12-13 21:49:24 +0700

    Add Rest Framework

--- app/example/settings.py
+++ app/example/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
