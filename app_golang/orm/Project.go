856727741008c9cd26c044ac43c8442f0ec5919e
Han-Wanburhan <Hangster10761@gmail.com>
2023-12-26 22:05:00 +0700

    sdasdsadas

--- app_golang/orm/Project.go
+++ app_golang/orm/Project.go
@@ -1,3 +1,19 @@
+// package orm
+
+// import (
+// 	"gorm.io/gorm"
+// )
+
+// type Project struct {
+//   gorm.Model
+//   screen_IMG string
+//   project_name string
+//   project_Path string
+//   WorkspaceID uint
+//   Workspace   Workspace `gorm:"foreignKey:WorkspaceID"`
+
+// }
+
 package orm
 
 import (
@@ -5,11 +21,10 @@ import (
 )
 
 type Project struct {
-  gorm.Model
-  screen_IMG string
-  project_name string
-  project_Path string
-  WorkspaceID uint
-  Workspace   Workspace `gorm:"foreignKey:WorkspaceID"`
-
+	gorm.Model
+	WorkspaceID  uint
+	Workspace    Workspace `gorm:"foreignKey:WorkspaceID"`
+  ScreenIMG    string    `gorm:"column:screen_img"`
+	ProjectName  string    `gorm:"column:project_name"`
+	ProjectPath  string    `gorm:"column:project_path"`
 }
