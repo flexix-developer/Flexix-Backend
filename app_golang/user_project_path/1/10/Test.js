5ed5ad1735a5c849c77672a7a8e61e0cc55bac58
Han-Wanburhan <Hangster10761@gmail.com>
2024-02-17 22:36:29 +0700

    []

--- app_golang/user_project_path/1/10/Test.js
+++ app_golang/user_project_path/1/10/Test.js
@@ -1,104 +1,39 @@
-const post = async () => {
-  const atnameValue = document.getElementById("input-3").value;
+const add_book = async () => {
+  // รับค่าจาก input fields
+  const atname = document.getElementById("input-3").value;
+
+  // สร้าง object สำหรับส่งข้อมูล
+  const dataBody = {
+    atname: atname,
+  };
 
   try {
     const response = await fetch("http://127.0.0.1:5000/api/author", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        atname: atnameValue,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Data:", data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-
-const gost = async () => {
-  const atnameValue = document.getElementById("input-3").value;
-
-  try {
-    const response = await fetch("http://127.0.0.1:5000/api/author", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        atname: atnameValue,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Data:", data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-const bad = async () => {
-  const atnameValue = document.getElementById("input-3").value;
-
-  try {
-    const response = await fetch("http://127.0.0.1:5000/api/author", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        atname: atnameValue,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Data:", data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-const sssost = async () => {
-  const atnameValue = document.getElementById("input-3").value;
-
-  try {
-    const response = await fetch("http://127.0.0.1:5000/api/author", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        atname: atnameValue,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Data:", data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-const get_books = async () => {
-  try {
-    const response = await fetch("http://127.0.0.1:5000/token/api/book?id=1", {
-      method: "GET",
       headers: {
         "Content-Type": "application/json", // กำหนด Content-Type header เป็น application/json
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDc3MzQyNzUsInVzZXJJZCI6MX0.vZCgeTAFCwyRsvRRy-cL7zTxUxokucy9gtLHAviwEkI",
+      },
+      body: JSON.stringify(dataBody), // แปลง object เป็นสตริง JSON
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json(); // อ่าน JSON จาก response body
+    console.log("Data:", data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+const getall = async () => {
+
+  try {
+    const response = await fetch("http://127.0.0.1:5000/api", {
+      method: "GET",
+            headers: {
+        "Content-Type": "application/json", // กำหนด Content-Type header เป็น application/json
+        
       },
     });
 
@@ -112,3 +47,4 @@ const get_books = async () => {
     console.error("Error fetching data:", error);
   }
 };
+
